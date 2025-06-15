12c73d2a741279a4926c0dfe6c133157bf7550cc
Humaidu Ali <35281323+Humaidu@users.noreply.github.com>
2025-06-15 13:16:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def profile():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False, host="0.0.0.0", port=5051)
+    app.run(debug=False, host="0.0.0.0")
 
